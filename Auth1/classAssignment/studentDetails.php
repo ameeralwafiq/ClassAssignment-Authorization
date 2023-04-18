40b381642ee9f9aaa8862d02ad96435da096c07b
Ameer Al-Wafiq <92366023+ameeralwafiq@users.noreply.github.com>
2023-04-18 23:08:17 +0800

    Update studentDetails.php

--- Auth1/classAssignment/studentDetails.php
+++ Auth1/classAssignment/studentDetails.php
@@ -5,15 +5,19 @@
 	<script src="js/validation.js"></script>
 </head>
 <body>
-
 	<?php 
 		// Start session and check if user is authorized
 		session_start();
 		if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
 			echo "You are not authorized to access this page. Please log in first.";
+	?>
+		<a href="login.php"><button>Log In</button></a>
+	<?php
 			exit();
 		}
 	?>
+	
+	
 	<h2>Registration Form</h2>
 	<form name="registration_form" method="post" action="submit_details.php" onsubmit="return validateInput();">
 
